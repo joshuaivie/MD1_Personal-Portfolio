9274de07e9e6baf960e5a5913e3f5fc2664cf573
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-01-24 22:23:06 +0100

    Add open-menu styling

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const menuButton = document.getElementById('menuButton');
+
+/* Scroll To Top */
+const scrollButton = document.getElementById('scrollToTop');
+
+const scrollFunction = () => {
+  if (
+    document.body.scrollTop > 300
+    || document.documentElement.scrollTop > 300
+  ) {
+    scrollButton.style.display = 'block';
+  } else {
+    scrollButton.style.display = 'none';
+  }
+};
+
+// eslint-disable-next-line func-names
+window.onscroll = function () {
+  scrollFunction();
+};
+
+const topFunction = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
