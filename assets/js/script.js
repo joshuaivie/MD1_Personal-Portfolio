4ebf70281bf9ca50e50dd7ac4d8b329ff22b9f54
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-01-26 01:31:15 +0100

    Add dynamic content rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/* eslint-disable max-len */
+
+// 1. Scroll To Top
 const scrollButton = document.getElementById('scrollToTop');
 
 const scrollFunction = () => {
@@ -11,7 +14,6 @@ const scrollFunction = () => {
   }
 };
 
-// eslint-disable-next-line func-names
 window.onscroll = function () {
   scrollFunction();
 };
@@ -21,34 +23,57 @@ const topFunction = () => {
   document.documentElement.scrollTop = 0;
 };
 
-/* menu-mobile */
-// const menuToggle = document.getElementById('menuToggle');
+// 2. Mobile Menu
 const menuWrapper = document.getElementById('menuWrapper');
 
 const menuMob = () => {
   menuWrapper.classList.toggle('open-menu');
 };
 
-const myFunc = menuMob;
-const myFunc2 = topFunction;
-console.log(myFunc, myFunc2);
+// 3. Project Details PopUp
+const getDetailsList = fetch('./assets/data/data.json').then((result) => result.json());
 
-const pdContainer =document.getElementById('pdContainer');
-const pdClose = document.getElementById('pdClose');
+const pdContainer = document.getElementById('pdContainer');
+const pdTitle = document.getElementById('pdTitle');
+const pdCompany = document.getElementById('pdCompany');
+const pdRole = document.getElementById('pdRole');
+const pdYear = document.getElementById('pdYear');
+const pdImage = document.getElementById('pdImage');
+const pdDescription = document.getElementById('pdDescription');
+const pdTechnologies = document.getElementById('pdTechnologies');
+const pdLive = document.getElementById('pdLive');
+const pdSource = document.getElementById('pdSource');
 
-const popUp = (id) => {
+const hasClass = (element, className) => (` ${element.className} `).indexOf(` ${className} `) > -1;
 
-  if(pdContainer.classList.contains('show')){
-    alert('show');
+const renderDetails = (title, company, role, year, imageName, description, technologies, live, github) => {
+  pdTitle.innerHTML = title;
+  pdCompany.innerHTML = company;
+  pdRole.innerHTML = role;
+  pdYear.innerHTML = year;
+  pdImage.src = `./assets/img/work-details/${imageName}`;
+}
 
-  }
-  else {
+const openDetails = (id) => {
+  if (!hasClass(pdContainer, 'show')) {
     pdContainer.classList.add('show');
-   
   }
 
-}
+  getDetailsList
+    .then((list) => list.find((project) => project.id === id) || list[0])
+    .then((d) => renderDetails(d.title, d.company, d.role, d.year, d.imageName));
+};
 
-const closeBtn = ()=>{
-  pdContainer.classList.remove('show');
-}
+const closeDetails = () => {
+  if (hasClass(pdContainer, 'show')) {
+    pdContainer.classList.remove('show');
+  }
+};
+
+const func1 = menuMob;
+const func2 = topFunction;
+const func3 = renderDetails;
+const func4 = closeDetails;
+// console.log(func1, func2, func3, func4);
+
+
